b93999568e4f3aafd1314b6101d53a6c8cb25efd
JangWooJin1 <jangwoojin1@gmail.com>
2024-12-04 19:49:49 +0900

    feat: searchQuery나 setSearchQuery가 없을 때 검색창 숨김 처리

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,17 +19,24 @@ const Header = ({
   onSelectLocation,
 }) => {
   const [placeholder, setPlaceholder] = useState('검색어 입력');
+
   const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
+    if (setSearchQuery) {
+      setSearchQuery(event.target.value);
+    }
   };
 
   const handleResultClick = (result) => {
-    onSelectLocation({
-      latitude: result.latitude,
-      longitude: result.longitude,
-      name: result.name,
-    });
-    setSearchQuery('');
+    if (onSelectLocation) {
+      onSelectLocation({
+        latitude: result.latitude,
+        longitude: result.longitude,
+        name: result.name,
+      });
+    }
+    if (setSearchQuery) {
+      setSearchQuery('');
+    }
     setPlaceholder(result.name);
   };
 
@@ -43,16 +50,12 @@ const Header = ({
             src="/main_logo.png"
             alt="SOL집모아 로고"
             className={custom_header.header}
-            onClick={() => {
-              navigate('/main');
-            }}
+            onClick={() => navigate('/main')}
             style={{ width: '40px', height: '40px', cursor: 'pointer' }}
           />
           <Navbar.Brand
             className="ms-2"
-            onClick={() => {
-              navigate('/main');
-            }}
+            onClick={() => navigate('/main')}
             style={{
               color: '#6f42c1',
               fontWeight: 'bold',
@@ -63,43 +66,45 @@ const Header = ({
             SOL집모아
           </Navbar.Brand>
         </div>
-        <Form
-          className="d-flex flex-grow-1 position-relative mx-lg-auto mb-2 mb-lg-0"
-          style={{
-            maxWidth: '500px',
-            minWidth: '300px',
-          }}
-        >
-          <div className="position-relative w-100">
-            <Search
-              style={{ position: 'absolute', top: '10px', left: '10px' }}
-            />
-            <Form.Control
-              type="text"
-              placeholder={placeholder}
-              value={searchQuery}
-              onChange={handleSearchChange}
-              style={{ paddingLeft: '30px', borderRadius: '25px' }}
-            />
-            {searchQuery && searchResults.length > 0 && (
-              <Dropdown.Menu
-                show
-                style={{ width: '100%', position: 'absolute', top: '40px' }}
-              >
-                {searchResults.map((result, index) => (
-                  <Dropdown.Item
-                    key={index}
-                    onClick={() => handleResultClick(result)}
-                  >
-                    {result.name} - {result.address}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            )}
-          </div>
-        </Form>
+        {/* 검색창 조건부 렌더링 */}
+        {searchQuery !== undefined && setSearchQuery && (
+          <Form
+            className="d-flex flex-grow-1 position-relative mx-lg-auto mb-2 mb-lg-0"
+            style={{
+              maxWidth: '500px',
+              minWidth: '300px',
+            }}
+          >
+            <div className="position-relative w-100">
+              <Search
+                style={{ position: 'absolute', top: '10px', left: '10px' }}
+              />
+              <Form.Control
+                type="text"
+                placeholder={placeholder}
+                value={searchQuery}
+                onChange={handleSearchChange}
+                style={{ paddingLeft: '30px', borderRadius: '25px' }}
+              />
+              {searchQuery && searchResults && searchResults.length > 0 && (
+                <Dropdown.Menu
+                  show
+                  style={{ width: '100%', position: 'absolute', top: '40px' }}
+                >
+                  {searchResults.map((result, index) => (
+                    <Dropdown.Item
+                      key={index}
+                      onClick={() => handleResultClick(result)}
+                    >
+                      {result.name} - {result.address}
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              )}
+            </div>
+          </Form>
+        )}
         <div className="d-flex align-items-center justify-content-end flex-grow-1">
-          {/* 마이페이지 아이콘 */}
           <OverlayTrigger
             placement="bottom"
             overlay={<Tooltip>마이페이지</Tooltip>}
@@ -107,13 +112,10 @@ const Header = ({
             <img
               src="/mypage_icon.png"
               alt="마이페이지 아이콘"
-              onClick={() => {
-                navigate('/mypage');
-              }}
+              onClick={() => navigate('/mypage')}
               style={{ width: '40px', height: '40px', cursor: 'pointer' }}
             />
           </OverlayTrigger>
-          {/* 토큰 발행 아이콘 */}
           <OverlayTrigger
             placement="bottom"
             overlay={<Tooltip>토큰 발행</Tooltip>}
@@ -121,9 +123,7 @@ const Header = ({
             <img
               src="/coin_logo.png"
               alt="토큰 발행 아이콘"
-              onClick={() => {
-                navigate('/property_create');
-              }}
+              onClick={() => navigate('/property_create')}
               style={{
                 width: '38px',
                 height: '38px',
